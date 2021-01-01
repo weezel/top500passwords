8826e2959e0d42c8f46c3828be376b8b1ff203f9
Ville Valkonen <weezelding@gmail.com>
2021-01-01 22:07:06 +0200

    Migrate to Python3 and revamp the design.

--- checktop500passwords.py
+++ checktop500passwords.py
@@ -1,31 +1,35 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
 import hashlib
-import sys
+from typing import List
 
-fname = "top500passwords.txt"
+passwords_file = "top500passwords.txt"
 encrypts = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
 
-def doencrypt(s):
+
+def do_encrypt(s: str, enc: str) -> str:
     h = hashlib.new(enc)
-    h.update(s)
+    h.update(s.encode("utf-8"))
     return h.hexdigest()
 
 
-try:
-    F = open(fname, "r")
-    top500 = F.readlines()
-    F.close()
-except IOError:
-    print "Cannot find file: %" % fname
+def get_top500_common_passwds(filename: str) -> List[str]:
+    with open(filename, "r") as f:
+        data = f.readlines()
+    return data if data is not None else list()
 
 
-for x in top500:
-    # decrypted password
-    print "%s" % x.rstrip("\n"),
-    # encrypt password with different ways
-    for enc in encrypts:
-        print "(%s)%s" % (enc, doencrypt(x)),
-    print ""
+def main():
+    top500_passwds = get_top500_common_passwds(passwords_file)
+    for passwd in top500_passwds:
+        # Decrypted password
+        print("{}".format(passwd.rstrip("\n")))
+        # Encrypt password with different ciphers
+        for enc in encrypts:
+            print("(%s) %s" % (enc, do_encrypt(passwd, enc)))
+        print()
 
+
+if __name__ == '__main__':
+    main()
